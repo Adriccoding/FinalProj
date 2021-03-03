61388224661aafea6a5d8eaac7c09f7322c1594e
Adriccoding <63327083+Adriccoding@users.noreply.github.com>
2021-03-03 12:17:02 -0500

    Add files via upload

--- TheTavern-master/app/src/main/java/com/example/finalproj/MainMenu.java
+++ TheTavern-master/app/src/main/java/com/example/finalproj/MainMenu.java
@@ -2,7 +2,10 @@ package com.example.finalproj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -10,5 +13,26 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+    }
+
+    public void MoveCharacterAbridged(View view) {
+        Intent i = new Intent (this, CharacterAbridgedActivity.class);
+        startActivity(i);
+    }
+
+
+    public void Move_CharacterCreate(View view) {
+        Intent i = new Intent (this, NewCharacterActivity.class);
+        startActivity(i);
+    }
+
+    public void Move_Rules(View view) {
+        Intent i = new Intent (this, RulesActivity.class);
+        startActivity(i);
+    }
+
+    public void Move_ViewCharacters(View view) {
+        Intent i = new Intent (this, ViewCharacterActivity.class);
+        startActivity(i);
     }
 }
